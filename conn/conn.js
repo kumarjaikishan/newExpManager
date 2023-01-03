9a174c0c3d49db459b3b4d7ba0df7de79ceff0dc
Jai Kishan <72691369+kumarjaikishan@users.noreply.github.com>
2023-01-03 12:55:58 +0530

    dsfvd

--- conn/conn.js
+++ conn/conn.js
@@ -1,6 +1,6 @@
 const mongo = require('mongoose');
 const urie = require('./middle');
-const db =urie.url;
+const db =urie;
 // const db ='mongodb+srv://jai:Jai%404880@cluster0.4ntduoo.mongodb.net/exp?retryWrites=true&w=majority';
 // console.log(urie.uri);
 mongo.set('strictQuery', false);
